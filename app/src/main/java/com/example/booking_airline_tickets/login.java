b03d91a4c2461886f6ef245b07988499515950ed
ahmed diab <aquapet33@gmail.com>
2023-05-21 00:35:26 +0300

    final

--- app/src/main/java/com/example/booking_airline_tickets/login.java
+++ app/src/main/java/com/example/booking_airline_tickets/login.java
@@ -1,41 +1,71 @@
 package com.example.booking_airline_tickets;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class login extends AppCompatActivity {
-    EditText email,password;
-    Button btnlogin;
+    private DB d = new DB(this);
+    EditText username,password;
+    TextView signup;
+    Button loginbtn;
     ProgressBar bar;
+    public static String USERNAME;
+    public static boolean loginflag = false;
    // FirebaseAuth auth;
-
+   Intent intent ,home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //auth = FirebaseAuth.getInstance();
-
-        email = (EditText) findViewById(R.id.email);
+        intent =  new Intent(this, SignUp.class);
+        home = new Intent(this,Home.class);
+        username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
-        btnlogin = (Button) findViewById(R.id.loginbtn);
-        bar = findViewById(R.id.bar);
+        loginbtn = (Button) findViewById(R.id.loginbtn);
+        bar = (ProgressBar) findViewById(R.id.bar);
+        signup= (TextView) findViewById(R.id.SIGNUP);
+        SpannableString SIGNUP = new SpannableString(signup.getText());
+        ClickableSpan CS = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                startActivity(intent);
+            }
+        };
 
-        btnlogin.setOnClickListener(new View.OnClickListener() {
+        SIGNUP.setSpan(CS,7,10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        signup.setText(SIGNUP);
+        signup.setMovementMethod(LinkMovementMethod.getInstance());
 
+        loginbtn.setOnClickListener(new View.OnClickListener() {
                                         @Override
                                         public void onClick(View view) {
-                                            bar.setVisibility(View.VISIBLE);
-
-                                            String emailvalue = email.getText().toString();
-                                            String passvalue = password.getText().toString();
-
-                                            checkValidation();
+                            String USER_NAME = username.getText().toString();
+                            String PASSWORD = password.getText().toString();
+                            boolean auth = d.authenticateUser(USER_NAME,PASSWORD);
+                                            if (auth){
+                                                loginflag=true;
+                                                bar.setVisibility(View.VISIBLE);
+                                                startActivity(home);
+                                                USERNAME=USER_NAME;
+                                            } else {
+                                                AlertDialog.Builder dialog = new AlertDialog.Builder(login.this);
+                                                dialog.setTitle("انت ليس لديك حساب                  ").show();
+                                            }
 
 
                                         }
@@ -43,8 +73,8 @@ public class login extends AppCompatActivity {
         );
     }
     public void checkValidation(){
-        if(checkEmpty(email)){
-            email.setError("Empty email");
+        if(checkEmpty(username)){
+            username.setError("Empty email");
         }
         if(checkEmpty(password)){
             password.setError("Empty password");
